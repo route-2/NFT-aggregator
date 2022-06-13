176e5f79db853d25fd288334ece3e12ed4cbc2f6
route-2 <ruthurao@gmail.com>
2022-06-13 22:35:05 +0530

    land

--- pages/ExplorePage.tsx
+++ pages/ExplorePage.tsx
@@ -12,7 +12,7 @@ const ExplorePage = () => {
     <>
     <Flex height={"100vh"} bgGradient='linear(to-br, #0A0081, #000000)'> 
     
-      <Box rounded={"xl"} justifyContent={"center"} alignItems={'center'} bg={"gray.100"} padding={"15px"} height={"fit-content"} ml={"80px"} w={"80%"} mt={"40px"} > 
+      <Box rounded={"2xl"} justifyContent={"center"} alignItems={'center'} bg={"gray.100"} padding={"15px"} height={"fit-content"} ml={"80px"} w={"80%"} mt={"40px"} > 
       <Input  bg={"blue.200"}  rounded={"2xl"} placeholder='Search' w= {"600px"} />
      </Box>
 
--- pages/Homepage.tsx
+++ pages/Homepage.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex,Image, Img, Text } from '@chakra-ui/react';
+import { background, Box, Button, Flex,Image, Img, Text } from '@chakra-ui/react';
 import Navbar from './api/components/Navbar';
 import NotConnectedModal from './api/components/NotConnectedModal';
 import { useMetamask } from './api/components/context/metamsk.context';
@@ -20,7 +20,7 @@ const Homepage =()=>{
   
           
        
-        <Flex bgGradient='linear(to-br, #0A0081, #000000)' > 
+        <Flex backgroundImage={"https://i.ibb.co/kxq0prc/Mac-Book-Pro-16-1background.jpg"} backgroundRepeat={'no-repeat'} backgroundSize={'cover'} > 
         {/* <Flex w={'20px'} bgGradient='radial( yellow.400, pink.200)' > 
    
       
@@ -34,7 +34,7 @@ const Homepage =()=>{
             <Box width={"70%"} pl={'7%'} pt={"15%"} alignItems={"center"} justifyContent={"center"} height={"100vh"}  >
            <Box alignContent={"flex-start"}> 
            
-            <Text fontSize={"70px"} fontWeight={"bold"}  color={"white"}  textAlign={"start"}> Buy or trade your     
+            <Text fontSize={"6xl"} position={'absolute'} fontWeight={"bold"}  color={"white"}  > Buy or trade your     
             <Text  bgGradient={[
     'linear(to-tr, teal.500, yellow.400)',
     'linear(to-t, blue.500, teal.500)',
@@ -47,13 +47,15 @@ const Homepage =()=>{
             </Text> 
            
       </Box>
-                <Text fontSize={"2xl"} color={"white"} pb={"30px"}>To the social media of  NFT  creators</Text>
+                <Text fontSize={"2xl"} color={"white"} position={'absolute'} pb={"30px"} pt={"200px"}>To the social media of  NFT  creators</Text>
                 <Button 
                 bgGradient={[
                   'linear(to-tr, teal.300, yellow.400)',
                   'linear(to-t, blue.200, teal.500)',
                   'linear(to-b, orange.100, purple.800)',
-                ]}    
+                ]}   
+                mt={'260px'} 
+                size={"md"}
                 
                 
                 
@@ -68,19 +70,25 @@ const Homepage =()=>{
             {/* <Box justifyContent={"center"} pt={"15%"} alignItems={"center"} textAlign={'start'} flex={"1"} height={"100vh"} bgGradient='linear(to-bl, #0A0081, #000000)'>
                
             </Box> */}
-            <Flex pl={"200px"} > 
-        <ButtonGroup pt={'20px'} pr={"20px"}  >
+            <Flex pl={"60%"} > 
+          <Box pt={'40px'} pr={"20px"} > 
+        <ButtonGroup   >
                 <a href='../../Homepage' ><Button variant={"ghost"} fontSize={"xl"} textColor={"white"} colorScheme={"whiteAlpha"}>Home</Button></a>
                 <a href='../../ExplorePage' ><Button variant={"ghost"} fontSize={"xl"} textColor={"white"} colorScheme={"whiteAlpha"}>Explore</Button></a>
                 <Button variant={"ghost"} colorScheme={"whiteAlpha"} fontSize={"xl"} textColor={"white"} > Create</Button>
             </ButtonGroup> 
-            <Box pt={"20px"} pl={"20px"}> 
-            {isWalletConnected && walletAddress && chain && currentWallet ?(<><ConnectedModal/></>):<NotConnectedModal/>}
-            <Image rounded={"2xl"} pt={'40%'} pr={"60%"} w={"1000px"} src='https://s8.gifyu.com/images/WhatsApp-Video-2022-06-09-at-9.06.07-PM9d0aee11c2a7e0d3.gif'/>
             </Box>
+            <Box pt={"40px"} pr={"20px"} > 
+            {isWalletConnected && walletAddress && chain && currentWallet ?(<><ConnectedModal/></>):<NotConnectedModal/>}
+           
+            </Box>
+
             {/* <Box boxSize='sm'>
   <Image className='grad' src='https://i.ibb.co/S6Tz01f/Group-1gradient.png'  />
 </Box> */}
+<Box  boxSize={"sm"} pt={'220px'} marginRight={"100px"} > 
+<Image  src='https://s8.gifyu.com/images/WhatsApp-Video-2022-06-09-at-9.06.07-PM9d0aee11c2a7e0d3.gif'/>
+</Box>
 
 
 
